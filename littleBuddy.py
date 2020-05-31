77108e4d04ee0aa48eedd71efebcbeb6aacedfe9
ndrefg <48099550+ndrefg@users.noreply.github.com>
2020-05-31 01:11:04 +0100

    Rest to new sheet PT comments

--- littleBuddy.py
+++ littleBuddy.py
@@ -1,74 +1,67 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
-#-----Start Config----------------#
+# -----Start Config----------------#
 
-#list of values to search for in order (will first search for first then second and so on).
-# more can be added if you want 
-KEYS=["First","Second","Third"]
-
-#row to start inserting rows that are marked with keys 
-ITERATOR_START=5
-
-#row to start inserting rows that are unmarked
-UNDEF_START=50
-
-#name of the input file 
-#must be in the same directory as the .py file
-INPUT_FILE='sample_input1.xlsx'
-
-#name of the output file to be created or modified
-#if a file of this name does not exist then a new one will be created in the same directory as the .py file
-OUTPUT_FILE='sample_output1.xlsx'
-
-#column that contains the key values
-KEY_COL=0
+KEYS = ["Banana", "Cenas", "Cherries"] # Lista com os valores a procurar
 
 
-#-----End Config------------------#
+ITERATOR_START = 1 # row onde começa a escrever no resultado
+
+
+UNDEF_START = 2 # row onde mete os valores nao definidos
+
+
+INPUT_FILE = 'produceSales.xlsx' # Ficheiro original a ler
+
+
+OUTPUT_FILE = 'file2.xlsx' # Ficheiro a escrever e que vai ser criado se nao existir
+
+# Coluna onde estão os dados a procurar
+KEY_COL = 0
+
+# -----End Config------------------#
 
 
 try:
 
-    if ITERATOR_START>= UNDEF_START:
+    if ITERATOR_START >= UNDEF_START:
         raise Exception('iterator is greater than undef. This will result in data being overwritten in the output file')
-    iterator=ITERATOR_START
-    undef=UNDEF_START
-    wb = load_workbook(filename = INPUT_FILE)
-    sheets = wb.sheetnames
+    iterator = ITERATOR_START # define o interator com o valor da row onde começa a escrever no resultado
+    undef = UNDEF_START # reduz a variavel UNDEF_START definida em cima para undef
 
-    newWb = Workbook() 
-    newSheet= newWb.sheetnames
-    newWs=newWb[newSheet[0]]
+    wb = load_workbook(filename=INPUT_FILE) # abre o ficheiro de origem
+    sheets = wb.sheetnames # Abre as folhas do ficheiro de origem
 
+    newWb = Workbook() # Cria e abre ou só abre o ficheiro de destino
+    newSheet = newWb.sheetnames # Abre as folhas do ficheiro de destino
+    newWs = newWb[newSheet[0]] # Cria uma nova folha no ficheiro de destino
+    ws3 = newWb.create_sheet(title="Data")
 
-    ws=wb[sheets[0]]
-    for i in KEYS: 
-        for row in ws.iter_rows():
-        
-        
-            if row[KEY_COL].value==i:
-                for cell in row:
-                    newWs.cell(row=iterator, column=cell.column).value = cell.value
-                iterator+=1
-                if iterator>=UNDEF_START:
-                     raise Exception('Iterator has reached range reserved for undefined rows. Iterator: '+str(iterator)+' Undef_Start: '+str(UNDEF_START))
-        
-           
-           
-          
-    for row in ws.iter_rows():
-        if (row[KEY_COL].value not in KEYS ):
-            for cell in row:
-                newWs.cell(row=undef, column=cell.column).value = cell.value
-            undef+=1  
+    ws = wb[sheets[0]] # Chama a folha 0 do ficheiro de origem de "ws"
 
-    newWb.save(filename=OUTPUT_FILE)
-    
+    for i in KEYS: # cicla pelo numero de KEYS que estiverem na lista a procurar
+        for row in ws.iter_rows(): # itera pelas rows todas
+
+            if row[KEY_COL].value == i: # Se o valor na coluna a procurar da row for igual ao valor da lista
+                for cell in row: # Para o valor na celula da row faz
+                    newWs.cell(row=iterator, column=cell.column).value = cell.value #copia valores para ficheiro de destino
+                iterator += 1 # adiciona 1 ao iterador
+                # if iterator >= UNDEF_START: # se o iterador for maior que 50
+                #     raise Exception('Iterator has reached range reserved for undefined rows. Iterator: ' + str(
+                #         iterator) + ' Undef_Start: ' + str(UNDEF_START))
+
+    for row in ws.iter_rows(): # itera pelas rows todas
+        if row[KEY_COL].value not in KEYS: # se o valor na coluna a procurar da row nao estiver nas lista
+            for cell in row: # percorre as cell em pelas row
+                ws3.cell(row=undef, column=cell.column).value = cell.value # copia os valores para o ficheiro de destino
+            undef += 1 # adiciona 1 ao iterador
+    newWb.save(filename=OUTPUT_FILE) # guarda o ficheiro de destino
+
 except FileNotFoundError as e:
-        print("ERROR: Unable to open file. File may be outside of application directory") #Does not exist OR no read permissions
+    print(
+        "ERROR: Unable to open file. File may be outside of application directory")  # Does not exist OR no read permissions
 except PermissionError as e:
-    print("ERROR: Output file is open. Please close it and try again") 
+    print("ERROR: Output file is open. Please close it and try again")
 except Exception as e:
     print('ERROR: ', e)
-            
